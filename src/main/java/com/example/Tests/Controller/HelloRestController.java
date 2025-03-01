2a48394061879ef04b61cfcf88543117b3f0781b
Abhi1234cs21 <kamleshadajariya6252@gmail.com>
2025-03-01 21:11:28 +0530

    put request added

--- src/main/java/com/example/Tests/Controller/HelloRestController.java
+++ src/main/java/com/example/Tests/Controller/HelloRestController.java
@@ -29,6 +29,14 @@ public class HelloRestController {
     public String sayHelloPost(@RequestBody UserDTO user){
         return "Hello " + user.getFirstName()+" "+user.getLastName()+" from Bridgelabz";
     }
-
+    // PUT request with query parameter and params as path
+    // http://localhost:8080/hello/put/Abhishek?lastName=Adjariya (Hello Abhishek Adjariya from BridgeLabz)
+    @PutMapping("/put/{firstName}")
+    public String sayHelloPut(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
 }
+}
+
+
+
 
